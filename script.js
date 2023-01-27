4e4ddc216736b1f31a66f59239cd0045cc80f6d7
alessromero20 <109921467+alessromero20@users.noreply.github.com>
2023-01-27 00:28:11 +0000

    I added arrays list, task 7

--- script.js
+++ script.js
@@ -22,17 +22,54 @@ let displayLink = document.querySelector(".display-link");
 
 
 // task 7: create and populate an array to store your image urls. Create three more arrays. One to store your song names, one for the artists, and a last one for the song links.
+// you might have accidentally copied the image instead of the link to the image. if that's the link the image gave you, that's a very strange way to store image data these days, and one way to pull images in would be to download them and upload them to your project. 
+//if you paste it again I can show you how I figured that out. 
+// so the address of the image should be the same as the link, they're both terms for the URL of the image. URL standing for ... either Uniform Resource Locator, which tells your browser how to look something up. you can get the image url by right clicking and selecting 'copy image address' usually.
+//sick so if instead of copying image address, you click 'copy image', then trying to paste that might give you the big ol blob of text you had before. Might, I'm not totally sure. It didn't work when I did it, but otherwise I have no idea how you got the text you did before. 
+// ahhhh, that would do it. But yeah, that URL you have is exactly what we're looking to put in the imageURLs list.
 
-let imageURLs = [ ]
-let songNamesList = [ ]
-let artistsList = [ ]
-let songLinksList = [ ]
+//  I tried using a different image this time
+//Here's the image adress
+//https://upload.wikimedia.org/wikipedia/en/c/ce/Young_and_Beautiful_cover_art.jpg
+//
+let imageURLs = [ "https://upload.wikimedia.org/wikipedia/en/c/ce/Young_and_Beautiful_cover_art.jpg", 
+  
+]
+let songNamesList = [
+  "Young and Beautiful", 
+  "Bizcochito", 
+  "Titi Me Pregunto", 
+  "Penny Lane"
+  ]
+let artistsList = [
+  "Lana Del Rey", 
+  "Rosalia", 
+  "Bad Bunny", 
+  "The Beatles"
+  ]
+let songLinksList = [
+  "https://youtu.be/o_1aF54DO60", 
+  "https://youtu.be/aG5C32aATKc", 
+  "https://youtu.be/Cr8K88UcO0s",
+  "https://www.youtube.com/watch?v=z7syIxQCquo"
+  ]
 
+// you might find it easier to keep track of your list entries if you split them across multiple lines, ala:
+//  let myList = [
+//  'some',   // first entry
+//  'entries' // second entry
+//  ]
+// another thing that could be helpful, is using comments to take notes. for example, you can put comments on the same line as code
+// which could be helpful for tracking which youtube link is which. 
+// Note, you don't have to do these things, I'm just suggesting them because they're useful tools when you're programming. Comments are less for other people and more for you in the future, like, a couple weeks from now looking back and going 'what was i even thinking when I wrote this?'
+// you may want to  put the bracket on the same line as your let, as javascript can get... weird. 
+// another option if you don't want to fiddle with spaces around your code, is up in the top right of this text window, theres a little box with text lines in it next to the vertical ... , which says 'format your code. '
 
-
+//Got it
 
 //REFACTOR ARRAYS DAY 
 // task 11: comment out the arrays data.
+
 // task 12: create an object for each of your songs.
 // task 13: inside each object, add key/value pairs to store the image url, song name, artist, and song link.
 // task 14: create an array that stores all of the objects.
